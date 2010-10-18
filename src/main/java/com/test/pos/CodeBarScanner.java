066f8e09a28095910efd753a922f6535e1b6bb62
J. B. Rainsberger <me@jbrains.ca>
2010-10-18 14:03:41 +0300

    Added a .gitignore to avoid committing class files.

--- src/main/java/com/test/pos/CodeBarScanner.java
+++ src/main/java/com/test/pos/CodeBarScanner.java
@@ -1,19 +1,26 @@
 package com.test.pos;
 
+// SMELL Misnamed class. This is not the barcode scanner;
+// it is a class that receives barcode scanning events.
+// The first event is "barcode" which contains a string.
 public class CodeBarScanner {
 
 	private final Display display;
-	
+
 	public CodeBarScanner(final Display display){
 		this.display = display;
 	}
-	
+
+  // SMELL Duplication between the two bodies of
+  // the if statements here.
 	public void scan(String barCode) {
 		if(barCode == null){
 			display.print("some error");
 		}else if("invalid barcode".equals(barCode)){
 			display.print("some error");
 		}
+    // SMELL Duplication between this 100L and the
+    // 100L in the test.
 		display.print(100L);
 	}
 
--- src/test/java/com/test/pos/CodeBarScannerTest.java
+++ src/test/java/com/test/pos/CodeBarScannerTest.java
@@ -6,34 +6,43 @@ import static org.mockito.Mockito.verify;
 import org.junit.Test;
 
 public class CodeBarScannerTest {
-	
+
 	@Test
 	public void shouldDisplayErrorOnDisplayWhenWrongBarCodeIsPassedIn(){
 		Display display = mock(Display.class);
 		CodeBarScanner codeBarScanner = new CodeBarScanner(display);
-		
+
 		codeBarScanner.scan("invalid barcode");
-		
+
+    // When you scan a barcode with no price, please print
+    // the message "No product found with barcode [scanned-barcode]"
 		verify(display).print("some error");
 	}
-	
+
 	@Test
 	public void shouldDisplayErrorOnDisplayWhenCodeBarIsNull(){
 		Display display = mock(Display.class);
 		CodeBarScanner codeBarScanner = new CodeBarScanner(display);
-		
+
+    // The manual for the barcode scanner claims that the
+    // scanner will never send you null, but at worst, an
+    // empty string. You can decide whether to keep this test.
 		codeBarScanner.scan(null);
-		
+
+    // If you will provide an error message for this case, then
+    // I'd like a more detailed error message, such as
+    // "Scanning barcode seems to have failed: received null barcode."
 		verify(display).print("some error");
 	}
-	
+
 	@Test
 	public void shouldDisplayPriceForTheGivenBarCode(){
 		Display display = mock(Display.class);
 		CodeBarScanner codeBarScanner = new CodeBarScanner(display);
-		
+
+    // How do you know that the price should be 100?
 		codeBarScanner.scan("valid barcode");
-		
+
 		verify(display).print(100L);
 	}
 }
