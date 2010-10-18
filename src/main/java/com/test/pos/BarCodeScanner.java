29e8b18534de2ef5922da65f5f07f530850244ea
alinpopa <alin.popa@gmail.com>
2010-10-18 11:41:14 +0300

    Committed code before asked to delete it :)

--- src/main/java/com/test/pos/BarCodeScanner.java
+++ src/main/java/com/test/pos/BarCodeScanner.java
@@ -2,15 +2,22 @@ package com.test.pos;
 
 public class BarCodeScanner {
 
-	public static final long CODE_PRICE = 123L;
-	private final Display display;
+	public static final String CODE_01 = "code1";
+	public static final String CODE_02 = "code2";
+	public static final long CODE_PRICE_01 = 100L;
+	public static final long CODE_PRICE_02 = 200L;
 	
-	public BarCodeScanner(final Display display) {
+	private final Display display;
+	private final Repository repository;
+	
+	public BarCodeScanner(final Display display, final Repository repository) {
 		this.display = display;
+		this.repository = repository;
 	}
 
 	public void scan(final String barCode) {
-		display.print(CODE_PRICE);
+		final Long price = repository.get(barCode);
+		display.print(price);
 	}
 
 }
--- src/test/java/com/test/pos/BarCodeScannerTest.java
+++ src/test/java/com/test/pos/BarCodeScannerTest.java
@@ -1,20 +1,42 @@
 package com.test.pos;
 
-import static com.test.pos.BarCodeScanner.CODE_PRICE;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 import org.mockito.Mockito;
 
-
 public class BarCodeScannerTest {
 
 	@Test
-	public void priceShouldBeDisplayed() throws Exception {
-		Display display = Mockito.mock(Display.class);
-		BarCodeScanner barCodeScanner = new BarCodeScanner(display);
+	public void priceShouldBeDisplayedForOneCode() throws Exception {
+		Display display = mock(Display.class);
+		Repository repository = mock(Repository.class);
+		when(repository.get("barcode")).thenReturn(100L);
+		BarCodeScanner barCodeScanner = new BarCodeScanner(display,repository);
 
-		barCodeScanner.scan("code");
+		barCodeScanner.scan("barcode");
 
-		Mockito.verify(display).print(CODE_PRICE);
+		Mockito.verify(display).print(100L);
+	}
+
+	@Test(expected = InvalidBarCodeException.class)
+	public void shouldHandleNullBarCode() throws Exception{
+		Display display = mock(Display.class);
+		Repository repository = mock(Repository.class);
+		when(repository.get(null)).thenThrow(new InvalidBarCodeException());
+		BarCodeScanner barCodeScanner = new BarCodeScanner(display,repository);
+
+		barCodeScanner.scan(null);
+	}
+	
+	@Test(expected = InvalidBarCodeException.class)
+	public void shouldHandleInvalidBarCode() throws Exception{
+		Display display = mock(Display.class);
+		Repository repository = mock(Repository.class);
+		when(repository.get("invalid code")).thenThrow(new InvalidBarCodeException());
+		BarCodeScanner barCodeScanner = new BarCodeScanner(display,repository);
+
+		barCodeScanner.scan("invalid code");
 	}
 }
