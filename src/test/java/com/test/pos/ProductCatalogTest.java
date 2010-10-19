c88ddb2779fb9dfd39ce9feeca0d70abee8d9680
alinpopa <alin.popa@gmail.com>
2010-10-19 21:51:04 +0300

    Change name of the test (not testing the interface, but the concrete implementation)

--- src/test/java/com/test/pos/ProductCatalogTest.java
+++ src/test/java/com/test/pos/ProductCatalogTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 
-public class CatalogTest {
+public class ProductCatalogTest {
 	
 	@Test
 	public void firstProductRetrievedShouldBeTheFirstThatWasSetInCatalog(){
